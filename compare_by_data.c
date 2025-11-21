3ff16f19940540ac419141eff03aeabf941df65e
asibe <asibe@ubuntu>
2025-11-21 10:40:43 +0100

    update: modif de delte move et creation d'un main

--- compare_by_data.c
+++ compare_by_data.c
@@ -26,15 +26,3 @@ bool compare(const char * file1, const char * file2){
     unsigned char c2 = checksum(file2);
     return c1 == c2;
 }
-
-int main(int argc,char * argv[]){
-
-    if(argc != 3){
-	printf("erreur");
-	return -1;
-    }
-    if(compare(argv[1],argv[2])){
-        printf("true\n");
-    }
-    return 0;
-}
